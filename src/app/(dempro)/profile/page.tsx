0631adeb884254898aa4a527dde52e3b77041e1f
Arbi Hysenaj <hysenajarbi@gmail.com>
2025-07-03 13:09:18 +0200

    feature:profile is now a protected route, if user is not authenticated, he will be redirected to signin

--- src/app/(dempro)/profile/page.tsx
+++ src/app/(dempro)/profile/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { redirect } from "next/navigation";
 
-function Profile() {
+import { getServerUser } from "@/lib/api-helpers";
+
+export default async function Profile() {
+  const { user } = await getServerUser();
+  if (!user) {
+    redirect("/signin");
+  }
   return <div className="text-3xl mt-8">Profile</div>;
 }
-
-export default Profile;
